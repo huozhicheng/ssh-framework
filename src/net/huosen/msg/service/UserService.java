70b8a2860ec9b74dba5e45288595a0ac6b25759b
huozhicheng <huozhicheng@gmail.com>
2013-04-30 22:42:44 +0800

    将注解事务改为xml配置方式

--- src/net/huosen/msg/service/UserService.java
+++ src/net/huosen/msg/service/UserService.java
@@ -8,7 +8,7 @@ import net.huosen.msg.dao.UserDao;
 import net.huosen.msg.model.User;
 
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+
 /**
  * @Description: Service层，在此可以进行事物管理
  * @Author huozhicheng@gmail.com
@@ -17,33 +17,33 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Component("userService")
 public class UserService {
-	@Resource(name="userDao")
+	@Resource(name = "userDao")
 	private UserDao dao;
-	@Transactional
-	public boolean insertUser(User user){
+
+	public boolean insertUser(User user) {
 		dao.insertUser(user);
 		return true;
 	}
-	@Transactional
-	public boolean deleteById(long id){
+
+	public boolean deleteById(long id) {
 		dao.deleteById(id);
 		return true;
 	}
-	@Transactional
-	public boolean  updateUser(User user){
+
+	public boolean updateUser(User user) {
 		dao.Update(user);
 		return true;
 	}
-	@Transactional
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return dao.findAll();
 	}
-	@Transactional
-	public List<User> findByName(String name){
+
+	public List<User> findByName(String name) {
 		return dao.findByName(name);
 	}
-	@Transactional
-	public User findById(long id){
+
+	public User findById(long id) {
 		return dao.findById(id);
 	}
-	}
+}
